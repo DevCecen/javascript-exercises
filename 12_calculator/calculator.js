6c6931e407ca1d35d238b36736ee4d914d6e1d4d
DevCecen <191208060+DevCecen@users.noreply.github.com>
2025-06-22 21:28:12 +0300

    Add solution 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(...theArgs) {
+	return theArgs.reduce((acc,index) => acc + index,0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...theArgs) {
+  return theArgs.slice(1).reduce((acc,index) => acc - index,theArgs[0]);
 };
 
-const sum = function() {
-	
+const sum = function(theArgs) {
+  let sum = 0;
+  for(let i of theArgs)
+  {
+    sum += +i;
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(theArgs) {
+  return theArgs.reduce((acc,index) => acc * index,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fac = 1;
+	for(let i = 1; i<=a; i++){
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line

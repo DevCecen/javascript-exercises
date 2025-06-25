00c2fa53628b7cbe63ce073adf83992fd56f6192
DevCecen <191208060+DevCecen@users.noreply.github.com>
2025-06-25 15:56:10 +0300

    Add solutions getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(a) {
+    const titles = a.map(function(kitap){
+        return kitap.title
+    })
+    return titles
 };
 
 // Do not edit below this line

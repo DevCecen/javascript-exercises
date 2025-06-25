b5df84836cb1cb85e4de52d44986b2e39e2fd68a
DevCecen <191208060+DevCecen@users.noreply.github.com>
2025-06-25 14:58:16 +0300

    Add solutions fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if(typeof value === "string") value = Number(value);
+    if(value<0) return "OOPS";
+    let counter = 0;
+    if(value === counter)
+        return 0;
+    let a = 1;
+    counter++;
+    if(value === counter)
+        return a
+    let b = a;
+    counter++;
+    if(value === counter)
+        return b
 
+    for(let i = 0; i<=value; i++)
+    {
+        let c = a+b;
+        counter++;
+        if(value == counter)
+            return c
+        let d = c + b;
+        counter ++;
+        if(value == counter)
+            return d;
+
+        a = c;
+        b = d;
+    }
 };
 
 // Do not edit below this line
